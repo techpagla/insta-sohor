3c029d5f91942809c9e8dc60ebd2adcdc7a4b3ab
thedarkhunterasif <iasif619@gmail.com>
2023-01-20 11:44:30 +0600

    The profile picture of the user is fixed

--- js/app.js
+++ js/app.js
@@ -51,7 +51,8 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-    const image = post.image;
+  const profileImage = post.userImage;  
+  const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${profileImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
